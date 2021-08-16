171f065718ed9a189c09c9a0d0c698e2ef137a38
Aguistin Lessa <gstnlss@protonmail.com>
2021-08-16 17:29:37 -0300

    nvim: specify neovim node package manually or else nvim bugs out sometimes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,7 @@ require('keymaps')
 
 vim.api.nvim_set_var('python_host_prog', '/usr/bin/python2')
 vim.api.nvim_set_var('python3_host_prog', '/usr/bin/python3')
+vim.api.nvim_set_var('node_host_prog', '/usr/local/bin/neovim-node-host')
 
 vim.api.nvim_set_var('NERDTreeGitStatusUseNerdFonts', 1)
 
@@ -13,37 +14,37 @@ vim.api.nvim_set_var('airline_theme', 'gruvbox')
 vim.api.nvim_set_var('airline_powerline_fonts', 1)
 
 -- custom/mine
-vim.api.nvim_set_keymap('n', '<Leader>ss', ':w<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>sa', ':wa<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>ss', ':w<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>sa', ':wa<CR>', { noremap = true })
 
 -- TODO: move this to it's own file
 require'compe'.setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = 'enable',
-    throttle_time = 80,
-    source_timeout = 200,
-    resolve_timeout = 800,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = {
-        border = {'', '', '', ' ', '', '', '', ' '}, -- the border option is the same as `|help nvim_open_win|`
-        winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-        max_width = 120,
-        min_width = 60,
-        max_height = math.floor(vim.o.lines * 0.3),
-        min_height = 1
-    },
-    source = {
-        path = true,
-        buffer = true,
-        nvim_lsp = true,
-        nvim_lua = true,
-        vsnip = true
-    }
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  resolve_timeout = 800,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = {
+    border = { '', '', '', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = 'NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder',
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1
+  },
+  source = {
+    path = true,
+    buffer = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    vsnip = true
+  }
 }
 
