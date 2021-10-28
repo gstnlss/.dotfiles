123d04eabb0ce1e7956e2227139a616d2a80ed29
Agustin Lessa <agustin@lessa.dev>
2021-10-28 14:06:03 -0300

    nvim: add support for c# lsp

--- nvim/.config/nvim/lua/lsp_settings/init.lua
+++ nvim/.config/nvim/lua/lsp_settings/init.lua
@@ -8,3 +8,4 @@
 require('lsp_settings.efm_langserver')
 require('lsp_settings.sumneko_lua')
 require('lsp_settings.tsserver')
+require('lsp_settings.omnisharp_roslyn')
---
+++ nvim/.config/nvim/lua/lsp_settings/omnisharp_roslyn.lua
@@ -0,0 +1,11 @@
+local lspconfig = require('lspconfig')
+local on_attach = require('lsp_settings.utils').on_attach
+local pid = vim.fn.getpid()
+
+local omnisharp_bin = '/usr/bin/omnisharp'
+
+lspconfig.omnisharp.setup {
+  cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
+  on_attach = on_attach
+}
+
