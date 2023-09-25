00e372404d2986d842a557e1228f2143c4388618
Agustin Lessa <agustinlessa@pm.me>
2023-09-25 11:23:52 -0300

    nvim: add autos-session pre_save hooks to cleanup and avoid bugs

--- nvim/.config/nvim/after/plugin/auto-session.lua
+++ nvim/.config/nvim/after/plugin/auto-session.lua
@@ -1,8 +1,22 @@
+local function close_nvim_tree()
+  vim.cmd [[NvimTreeClose]]
+end
+
+local function close_all_floating_wins()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= '' then
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+end
+
 require('auto-session').setup {
   log_level = 'error',
   auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-  auto_restore_enabled = false,
-  bypass_session_save_file_types = { 'NvimTree', 'alpha' }
+  auto_restore_enabled = true,
+  bypass_session_save_file_types = { 'NvimTree', 'alpha' },
+  pre_save_cmds = { close_all_floating_wins, close_nvim_tree }
 }
 
 vim.keymap.set(
