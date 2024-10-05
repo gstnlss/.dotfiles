2ab111443b52f9fa4039b2617c9a733d50b1fd67
Agustin Lessa <agustinlessa@pm.me>
2024-10-05 00:07:32 -0300

    zsh: update default node version and nvm script

--- zsh/.config/zsh/nvm.zsh
+++ zsh/.config/zsh/nvm.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 export NVM_DIR="$HOME/.local/share/nvm"
-DEFAULT_NODE_VERSION="v18"
+DEFAULT_NODE_VERSION="v20"
 
 if [[ ! -d "$NVM_DIR" ]] then
   mkdir -p "$NVM_DIR"
-  PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash'
+  PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash'
 fi
 
 [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
